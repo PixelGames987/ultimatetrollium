0e518eb6e452f86441f5485389303ec306a84e46
Krzysztof Kuczewski <92270732+PixelGames987@users.noreply.github.com>
2025-10-06 08:30:51 +0200

    Update pixie-all.sh

--- pixie-all.sh
+++ pixie-all.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+mode=$(iwconfig "${INTERFACE}" | grep -o "Mode:Monitor")
+
+if [[ "${mode}" = "Mode:Monitor" ]]; then
+        sudo ifconfig ${INTERFACE} down
+        sudo iwconfig ${INTERFACE} mode managed
+        sudo ifconfig ${INTERFACE} up
+fi
+
 export absolute_path=$(pwd)
 
 cleanup() {
